512e98d0bb5458cf3642038f4db1ba3c59b96640
devpushpendrakumar <devpushpendrakumar@gmail.com>
2025-06-26 16:28:33 +0530

    <Fix>:Change the animation to get reaction time

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 // Function to change box color and display reaction time.
 const colorChanger = () => {
-  timeBoard.classList.remove("color-change");
-  void timeBoard.offsetWidth;
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  box.style.backgroundColor = randomColor;
-  endTime = Date.now();
-  const timeTaken = endTime - startTime;
-  startTime = Date.now(); // Reset start time for the next click
-  const text = `Latest reaction Time: ${(timeTaken / 1000).toFixed(2)} seconds`;
-  timeBoard.textContent = text;
-  timeBoard.classList.add("color-change");
+  console.log(hidden);
+  if (hidden) {
+    box.classList.add("hidden");
+    const timeDelay = randomNumber(0, 2000);
+    setTimeout(() => {
+      void box.offsetWidth;
+      box.classList.remove("hidden");
+      startTime = Date.now();
+      hidden = false;
+    }, timeDelay);
+    box.style.left = `${randomNumber(0, 350) + 25}px`;
+    box.style.top = `${randomNumber(0, 200) + 25}px`;
+  } else {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    box.style.backgroundColor = randomColor;
+    endTime = Date.now();
+    responseTime = ((endTime - startTime) / 1000).toFixed(2);
+    console.log(responseTime);
+    hidden = true;
+    timeBoard.classList.remove("color-change");
+    void timeBoard.offsetHeight;
+    timeBoard.classList.add("color-change");
+    timeBoard.innerText = `Your latest reaction time is ${responseTime} seconds`;
+  }
 };
 
-let startTime = Date.now();
+let startTime;
 let endTime;
+let responseTime;
+let hidden = true;
 
 const box = document.getElementById("box");
 box.addEventListener("click", colorChanger);
@@ -21,3 +37,8 @@ box.addEventListener("click", colorChanger);
 const timeBoard = document.getElementById("time-board");
 
 console.log("Script added");
+// Helper functions
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
